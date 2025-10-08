cf600879b1bab76f4b8760e05efc98a2cbde4082
PancitoDulce16 <164936770+PancitoDulce16@users.noreply.github.com>
2025-10-07 23:14:14 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@ import { AppCore } from './core.js';
 import { ItineraryHandler } from './itinerary.js';
 import { TabsHandler } from './tabs.js';
 import { ModalRenderer } from './modals.js';
+import { MapHandler } from './map.js';
 
-// Inicializar la aplicación
 function initApp() {
   AppCore.init();
   ItineraryHandler.renderItinerary();
   TabsHandler.renderAllTabs();
+  MapHandler.renderMap();
   ModalRenderer.renderModals();
 }
 
-// Ejecutar cuando el DOM esté listo
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', initApp);
 } else {
