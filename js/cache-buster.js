d2fcfb45330a95080050a5b56df2e89f93d99beb
PancitoDulce16 <164936770+PancitoDulce16@users.noreply.github.com>
2025-11-11 16:44:12 -0600

    Force cache clear with new APP_VERSION for auto-correction fix

--- js/cache-buster.js
+++ js/cache-buster.js
@@ -1,7 +1,7 @@
 // js/cache-buster.js - Sistema de detección y corrección automática de cache
 // Este archivo FUERZA reloads cuando detecta archivos viejos
 
-const APP_VERSION = '2025-11-10-DISABLE-AUTORELOAD-FIX';
+const APP_VERSION = '2025-11-11-MASTER-OPTIMIZER-AUTO-CORRECTION-FIX';
 const VERSION_KEY = 'app_version';
 
 /**
